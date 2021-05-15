e76979b630d082e083ac9a40420827c8137bf517
stian <stianbekkelund@hotmail.com>
2021-05-15 15:39:55 +0200

    Purged unused animations & fix error
    Purged unused animations and fixed a bug on form-error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./public/**/*.html", "./public/**/*.js"],
+  purge: {
+    content: ["./public/**/*.html", "./public/**/*.js"],
+    options: {
+      keyframes: true,
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
